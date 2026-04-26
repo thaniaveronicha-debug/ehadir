a0a088fe0132c25c1adf4b474cee45d50ab787a5
SK LADANG TAKU | PDT 2 <dba7361.t1@m365a1.moe.edu.my>
2026-04-26 14:35:25 +0800

    Show full names and fix layout overflow

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4127115168" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1247859476" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
